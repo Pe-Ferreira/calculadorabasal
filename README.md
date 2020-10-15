2fe658fb959a6868d89c76a02687b0209b855abd
Pedro Ferreira <pedro.ferreira.da.silva@sap.com>
2020-10-14 21:55:49 -0300

    adicionar scroll para exibir resultado, ajuste no rodapé de copyright, adicionando readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Calculadora Basal
+## Uma forma simples de descobrir quantas calorias seu corpo consome por dia apenas para sobreviver
+
+Esse projeto nasceu com o objetivo de fazer os primeiros centavos com AdSense. Iremos chegar lá nos próximos dias :pray:
+
+Para descobrir sua TMB(Taxa Metabólica Basal) o usuário precisa inserir alguns dados como _idade, peso, altura e sexo_.
+
+É isso, bem básico, bem simples e direto ao ponto.
+
+Obrigado por ter lido até aqui :heart:
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function calculate() {
     var weight = document.forms["calculator"]["weight"].value;
     var height = document.forms["calculator"]["height"].value;
     var age = document.forms["calculator"]["age"].value;
+    
     if(sex === "") {
         alert("Por favor selecione uma opção na categoria Sexo.");
     } else if (sex === "m") {
@@ -11,10 +12,13 @@ function calculate() {
     } else if (sex === "f") {
         tmb = 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age);
     }
+    
     var resultDisplay = document.getElementById('resultDisplay');
     resultDisplay.hidden = false;
     resultDisplay.innerHTML = 'Seu gasto calórico diário é ' + tmb + "cal";
-  }
+    
+    scrollToResult();
+}
 
 function resetForm() {
     document.forms["calculator"]["sex"].value = "";
@@ -22,4 +26,8 @@ function resetForm() {
     document.forms["calculator"]["height"].value = "";
     document.forms["calculator"]["age"].value = "";
     document.getElementById('resultDisplay').hidden = true;
+}
+
+function scrollToResult() {
+    window.scrollTo(0, 300);
 }
