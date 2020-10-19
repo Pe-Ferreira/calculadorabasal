27faa066dc396f9bca3c554af9574f2570c7128e
Pedro Ferreira <pedro.ferreira.da.silva@sap.com>
2020-10-18 23:06:42 -0300

    enhancing bootstrap version, adding modal to show the result and adding global variable logic

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
+var tmb;
+var sex;
+var weight;
+var height;
+var age;
+
 function calculate() {
-    var tmb;
-    var sex = document.forms["calculator"]["sex"].value;
-    var weight = document.forms["calculator"]["weight"].value;
-    var height = document.forms["calculator"]["height"].value;
-    var age = document.forms["calculator"]["age"].value;
-    
-    if(sex === "") {
-        alert("Por favor selecione uma opção na categoria Sexo.");
-    } else if (sex === "m") {
-        tmb = 66 + (13.7 * weight) + (5 * height) - (6.8 * age);
-    } else if (sex === "f") {
-        tmb = 655 + (9.6 * weight) + (1.7 * height) - (4.7 * age);
+    this.sex = document.forms["calculator"]["sex"].value;
+    this.weight = document.forms["calculator"]["weight"].value;
+    this.height = document.forms["calculator"]["height"].value;
+    this.age = document.forms["calculator"]["age"].value;
+
+    if(hasEmptyFields()) {
+        alert("Por favor, preencha todos os campos.");
+        return;
+    } else if (this.sex === "m") {
+        this.tmb = 66 + (13.7 * this.weight) + (5 * this.height) - (6.8 * this.age);
+    } else if (this.sex === "f") {
+        this.tmb = 655 + (9.6 * this.weight) + (1.7 * this.height) - (4.7 * this.age);
     }
     
     var resultDisplay = document.getElementById('resultDisplay');
     resultDisplay.hidden = false;
-    resultDisplay.innerHTML = 'Seu gasto calórico diário é ' + tmb + "cal";
+    resultDisplay.innerHTML = 'Seu gasto calórico diário é ' + this.tmb + "cal";
     
-    scrollToResult();
-}
+    $('#result-modal').modal('show');
+};
 
 function resetForm() {
     document.forms["calculator"]["sex"].value = "";
@@ -26,8 +32,22 @@ function resetForm() {
     document.forms["calculator"]["height"].value = "";
     document.forms["calculator"]["age"].value = "";
     document.getElementById('resultDisplay').hidden = true;
-}
+};
 
 function scrollToResult() {
     window.scrollTo(0, 300);
-}
+};
+
+function hasEmptyFields() {
+    var result = false;
+    if (this.sex === ""){
+        result = true;
+    } else if (this.weight === ""){
+        result = true;
+    } else if (this.height === ""){
+        result = true;
+    } else if (this.age === ""){
+        result = true;
+    }
+    return result;
+};
